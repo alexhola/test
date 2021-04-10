f1cb377365fcf9a37f3de4d5b39b4f89c9afff9a
alexhola <82262163+alexhola@users.noreply.github.com>
2021-04-10 14:45:21 +0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,7 +2,7 @@ import imaplib
 import email
 import telebot
 
-bot = telebot.TeleBot('<TOKEN_BOT>')  # укажите ваш токен
+bot = telebot.TeleBot('<token_bot>')  # укажите ваш токен
 
 mail = imaplib.IMAP4_SSL('imap.gmail.com')
 mail.login('<account>@gmail.com', '<password>')  # ваш новый аккаунт @gmail
@@ -15,7 +15,7 @@ for x in range(i):
     latest_email_uid = data[0].split()[x]
     result, email_data = mail.uid('fetch', latest_email_uid, '(RFC822)')
     raw_email = email_data[0][1]
-    mail.store(latest_email_uid, '+FLAGS', 'Deleted')  # удаляем чтоб не было повторной отправки
+    mail.store(latest_email_uid, '+FLAGS', '\\Deleted')  # удаляем чтоб не было повторной отправки
     raw_email_string = raw_email.decode('utf-8')
     email_message = email.message_from_string(raw_email_string)  # получаем сырое письмо в виде строки
 
@@ -23,10 +23,11 @@ for part in email_message.walk():
     if part.get_content_type() == "text/html" or part.get_content_type() == "text/plain":
         body = part.get_payload(decode=True)
         s = (email_message['Subject'])
+        numb = s[5:16]  # Выделяем номер получателя 11 значный в виде 7XXXXXXXXXX с помощью среза
         mss = str(body.decode('unicode-escape'))  # декодируем тело письма и в Python3 отменяем unicode
-        abon = mss[45:58]  # выделяем номер отправителя
-        smss = mss[59:]  # выделяем тело СМС, а именно собственно само сообщение
-        soobsh = ('От:  ' + abon + ' \n' + 'текст:  ' + smss)  # форматируем для отправки
+        smss = mss[53:]  # выделяем тело СМС, а именно собственно само сообщение
+        smss1 = smss.replace("Content:", "", 1)   # Убираем из тела смс, слово "Content:"
+        soobsh=('Кому:  +' + numb + '\n' + 'От:  ' + smss1)  # форматируем для отправки
         bot.send_message(<chat_id>, soobsh)  # укажите ваш chat_id и отправляйте в телегу ваше СМС
     else:
         continue
